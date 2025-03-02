56f7af8d717cf9d526073f2c8d0685f3c2e30a8a
Oleksandr Aleksyeyv <150668618+joeblackA@users.noreply.github.com>
2025-03-02 23:16:50 +0200

    Add files via upload
    Create a bash script to find all users with /bin/bash CLI in a file password. Load script in to your GitHub repo an provide link as answer.

---
+++ binbash.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+clear
+echo "List of users with /bin/bash CLI in passwd using grep+awk"
+grep /bin/bash /etc/passwd | awk -F: '{print $1}'
+echo "-----------------"
+echo "List of users with /bin/bash CLI in passwd awk only"
+awk -F: '$7 == "/bin/bash" {print $1}' /etc/passwd
+echo "-----------------"
